b2dc4b75d55455b06cce2ac5f4219530a80d7bf4
Legendary Emoji <60050010+LegendaryEmoji@users.noreply.github.com>
2020-12-13 15:37:34 +0500

    Fill Info

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-exports.Token = "Nzg1MDk5ODY3MDU1ODQ5NDgy.X8y7Og.CkYoGlLVPbEjMHi6elJ4G-aB7Mg"; //Bot Token - Important
-exports.Owner = "576893842058641412"; //Bot Owner ID - Important
-exports.Default_Prefix = "A!"; //Bot Default Prefix (Examples: A!, !)- Important
-exports.Color = "BLUE"; //Bot All Embeds Color - Use CAPS For Name (Examples: BLUE, RANDOM) - Important
+exports.Token = ""; //Bot Token - Important
+exports.Owner = ""; //Bot Owner ID - Important
+exports.Default_Prefix = ""; //Bot Default Prefix (Examples: A!, !)- Important
+exports.Color = ""; //Bot All Embeds Color - Use CAPS For Name (Examples: BLUE, RANDOM) - Important
 exports.Support = ``; //Support Server Link - Never Gonna Give You Up (If No Link Provided)
 exports.Donate = `https://www.paypal.com/paypalme/legendemoji`; //Please Don't Change It, If You Want To Support Me <3
