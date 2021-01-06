5521c172ab2b68b2af8f788b99dca06312f98bb2
ahmadare1 <76630177+ahmadare1@users.noreply.github.com>
2021-01-06 20:21:28 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-exports.Token = ""; //Bot Token - Important
+exports.Token = "NzkzMjE0MjU5OTMyNTYxNDE5.X-pAWA.6fbkwTRtbKWHx__k4goDMKCSrnk"; //Bot Token - Important
 exports.Owner = ""; //Bot Owner ID - Important
 exports.Default_Prefix = ""; //Bot Default Prefix (Examples: A!, !)- Important
 exports.Color = ""; //Bot All Embeds Color - Use CAPS For Name (Examples: BLUE, RANDOM) - Important
